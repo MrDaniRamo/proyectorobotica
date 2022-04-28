a063647e8fa88fe94a3cc73993a72f9bfee8996f
Melchor Ramos <mel.daniel.ramos@gmail.com>
2022-04-27 19:33:12 -0500

    second progress

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,10 +5,10 @@ $(".container").on("click", '.agregar', function(e){
     var div2 = '</div>';
     var text1 = $("#newText").val();
     text1 = '<p>' + text1 + ' </p>';
-    var butCheck = '<button class = "checar"> check </button>' ;
+    //var butCheck = '<button class = "checar"> check </button>' ;
     var butDel = '<button class = "del"> delete </button>';
 
-    $("#btn_1").append(div1 + text1 + butCheck + butDel+ div2);
+    $("#btn_1").append(div1 + text1 +  butDel+ div2);
 })
 
 $("#btn_1").on("click",".checar", function(){
